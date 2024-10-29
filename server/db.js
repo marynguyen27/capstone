8fc7875031ecfbd820ffb66d478b784c27c16189
Mary Nguyen <marynguyen27@gmail.com>
2024-10-28 20:01:09 -0700

    connect new database

--- server/db.js
+++ server/db.js
@@ -1,12 +1,10 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  user: process.env.DB_USER || 'block37_user',
-  host:
-    process.env.DB_HOST ||
-    'dpg-crqbhidsvqrc73csmf90-a.oregon-postgres.render.com',
-  database: process.env.DB_NAME || 'block37',
-  password: process.env.DB_PASSWORD || 'NC4LTqaFWLUbl5zuJMU9CX2HyEmmxNRX',
+  user: process.env.DB_USER || 'capstone_o7rj_user',
+  host: process.env.DB_HOST || 'dpg-csg4nlbqf0us73e7coc0-a',
+  database: process.env.DB_NAME || 'capstone_o7rj',
+  password: process.env.DB_PASSWORD || 'VRmRkynUmkQPbqYncJNDkp6cbZFyvGp3',
   port: process.env.DB_PORT || 5432,
   ssl: true,
 });
